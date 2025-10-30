42b172d455f0efee2df103b55604ca48140502ff
MuraliJbk <murali@jbkinfotech.com>
2025-10-30 15:14:22 +0530

    test

--- html/js/modules/formidable.js
+++ html/js/modules/formidable.js
@@ -1,7 +1,34 @@
 class Formidable {
-  constructor() {
-    throw new Error('Form data is not found');
+  constructor() {}
+  error() {
+    document.addEventListener('DOMContentLoaded', function () {
+      let inputs = document.querySelectorAll(
+        '.frm_forms .frm_form_fields .frm_form_field input, .frm_forms .frm_form_fields .frm_form_field textarea'
+      );
+      // if (inputs.length === 0) return;
+      console.log('test form');
+      function errorHide(e) {
+        let errorDiv = e.target
+          .closest('.frm_blank_field')
+          .querySelector('.frm_error');
+        if (!errorDiv) return;
+        let value = e.target.value;
+        value.length > 0
+          ? (errorDiv.style.opacity = '0')
+          : (errorDiv.style.opacity = '1');
+      }
+      inputs.forEach((input) => {
+        if (input.value.length > 0) {
+          let errorDiv = input
+            .closest('.frm_blank_field')
+            ?.querySelector('.frm_error');
+          if (errorDiv) {
+            errorDiv.style.opacity = '0';
+          }
+        }
+        input.addEventListener('input', errorHide);
+      });
+    });
   }
-  error() {}
 }
 export const formidable = new Formidable();
