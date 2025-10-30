8f1f47674cdde33eaac05b3c4ac3e8208d264598
MuraliJbk <murali@jbkinfotech.com>
2025-10-30 12:58:56 +0530

    test

--- html/js/modules/formidable.js
+++ html/js/modules/formidable.js
@@ -1,27 +1,7 @@
 class Formidable {
-  constructor() {}
-  error() {
-    document
-      .querySelectorAll(
-        '.frm_form_field.vertical_radio input[type="radio"], .frm_form_field.vertical_radio input[type="checkbox"]'
-      )
-      .forEach(function (input) {
-        input.addEventListener('change', function () {
-          document
-            .querySelectorAll(
-              '.frm_form_field.vertical_radio input[type="radio"], .frm_form_field.vertical_radio input[type="checkbox"]'
-            )
-            .forEach(function (r) {
-              var label = r.parentElement;
-              label.style.color = '';
-            });
-
-          const label = this.parentElement;
-          if (this.checked) {
-            label.style.color = '#FFF';
-          }
-        });
-      });
+  constructor() {
+    throw new Error('Form data is not found');
   }
+  error() {}
 }
 export const formidable = new Formidable();
